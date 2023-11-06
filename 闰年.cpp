f7da98668923ebb18566f01eefda61daa75e21de
Fragileshs <145463763+Fragileshs@users.noreply.github.com>
2023-11-06 17:07:40 +0800

    Update 闰年.cpp

--- 闰年.cpp
+++ 闰年.cpp
@@ -1,11 +1,16 @@
 #include <stdio.h>
-int main()
-{	int a;
+int main(){
+	int a;
     printf("请输入年份：") ; 
 	scanf("%d",&a);
-	if (a%4==0&&a%100!=0||a%400==0)
-	    printf("闰年");
-	else
-	    printf("平年");
+	if(a<=0){
+	    printf("年份不符合要求");
+    }
+	else{
+		if(a%4==0&&a%100!=0||a%400==0)
+	      printf("闰年");
+	    else
+	      printf("平年");
+	}     
 	return 0 ;
 } 
